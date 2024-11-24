bb84da8a35ed4dc15f31b0eca396b3f4f31d38a7
Efrem <Efremdaniel27@gmail.com>
2024-11-24 19:49:45 +0100

    hemnet exercise

---
+++ Excercise/excercise0/1/Scripts/exploring_hemnet_exercise.sql
@@ -0,0 +1,68 @@
+-- 1. a) ingested data
+
+-- 1. b) 
+SELECT * FROM main.hemnet_data;
+
+-- 1. c)
+SELECT COUNT(*) FROM main.hemnet_data;
+
+-- testing percentage_different
+SELECT 6950000*0.07;
+-- = 486,500
+SELECT 6950000-6950000*0.07;
+
+-- 1. d)
+DESC TABLE main.hemnet_data;
+
+-- 1. e)
+SELECT
+	final_price,
+	asked_price,
+	area,
+	address
+FROM
+	main.hemnet_data
+ORDER BY
+	final_price DESC
+	LIMIT 5;
+
+-- 1. f)
+SELECT
+	final_price,
+	asked_price,
+	area,
+	address
+FROM
+	main.hemnet_data
+ORDER BY
+	final_price 
+	LIMIT 5;
+
+-- 1. g)
+SELECT
+	MIN(final_price) AS min,
+	MEDIAN(final_price) AS median,
+	AVG(final_price) AS mean, 
+	MAX(final_price) AS max
+FROM
+	main.hemnet_data hd;
+
+-- 1. i)
+SELECT
+	COUNT(DISTINCT commune)
+FROM
+	main.hemnet_data; 
+
+-- 1. j)
+SELECT
+	COUNT(*)/ 500 * 100 AS percentage_above_10million
+FROM
+	main.hemnet_data
+WHERE
+	final_price > 10000000; 
+
+
+
+
+
+
